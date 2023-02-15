1d9623bc232636919d39875dc5efebee17f7412e
Vyacheslav Onuchin <sla651@mail.ru>
2023-02-15 22:06:30 +1000

    Edited method bruteForce, indexOf, characterAt, generateBruteForce. Create method stopGenerating.

--- Pr2503/Sources/BruteForce/PasswordSelection.swift
+++ Pr2503/Sources/BruteForce/PasswordSelection.swift
@@ -7,45 +7,53 @@
 
 import Foundation
 
-func bruteForce(passwordToUnlock: String) {
-    let allowedCharacters = String.printable.map { String($0) }
+final class PasswordSelection {
 
-    var password: String = ""
+    private var isRuning = true
 
-    // Will strangely ends at 0000 instead of ~~~
-    while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-        password = generateBruteForce(password, fromArray: allowedCharacters)
-//             Your stuff here
-        print(password)
-        // Your stuff here
-    }
+    func bruteForce(passwordToUnlock: String) {
+        let allowedCharacters = String.printable.map { String($0) }
 
-    print(password)
-}
+        isRuning = true
+        var password = ""
 
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index])
-                               : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+        // Will strangely ends at 0000 instead of ~~~
+        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
+            password = generateBruteForce(password, fromArray: allowedCharacters)
+            //             Your stuff here
+            print(password)
+            // Your stuff here
         }
+
+        print(password)
     }
 
-    return str
+    func stopGenerating() {
+        isRuning = false
+    }
+
+    func indexOf(character: Character, _ array: [String]) -> Int {
+        array.firstIndex(of: String(character))!
+    }
+
+    func characterAt(index: Int, _ array: [String]) -> Character {
+        index < array.count ? Character(array[index]) : Character("")
+    }
+
+    func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+
+        return str
+    }
 }
